a91dacf743bcabaffda20f22283e964b1a4c4e02
uiojkl-123 <66120426+uiojkl-123@users.noreply.github.com>
2022-09-07 17:47:29 +0900

    firebase, auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { Redirect, Route } from 'react-router-dom';
+import { Redirect, Route, useHistory } from 'react-router-dom';
 import {
   IonApp,
   IonIcon,
   IonLabel,
+  IonLoading,
   IonRouterOutlet,
   IonTabBar,
   IonTabButton,
@@ -10,7 +11,7 @@ import {
   setupIonicReact
 } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { book, ellipse, square, triangle } from 'ionicons/icons';
 import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
@@ -33,21 +34,67 @@ import '@ionic/react/css/display.css';
 import './theme/variables.css';
 
 import './App.scss'
+import { getDoc, doc } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+import { useEffect, useRef, useState } from 'react';
+import { auth, db } from './serviece/firebase';
+import { useStore } from './store/store';
 
 setupIonicReact();
 
 
-const App: React.FC = () => (
-  <IonApp>
-    <IonReactRouter>
-        <IonRouterOutlet>
-          <Route exact path="/main" component={Home} />
-          <Route exact path="/">
-            <Redirect to="/main" />
-          </Route>
-        </IonRouterOutlet>
-    </IonReactRouter>
-  </IonApp >
-);
+const App: React.FC = () => {
+
+  const [loading, setLoading] = useState<boolean>(true)
+
+  const { setCurrentUser } = useStore();
+
+  const mountRef = useRef<boolean>(true)
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        const userSnapshot = await getDoc(doc(db, 'user', user.uid))
+        if (!userSnapshot.exists()) {
+          setLoading(false)
+          return
+        }
+        if (!userSnapshot) { return }
+        const userData = userSnapshot?.data()
+
+        if (mountRef.current) {
+          setCurrentUser({ userId: user.uid, ...userData })
+          setLoading(false)
+        }
+      } else {
+        setLoading(false)
+      }
+    });
+
+    return () => {
+      mountRef.current = false
+    }
+  }, [])
+
+  return (
+    <IonApp>
+      <IonReactRouter>
+        {loading ? (
+          <IonLoading
+            isOpen={true}
+            message={'기다려 주세요...'}
+          />
+        ) : (
+          <IonRouterOutlet>
+            <Route exact path="/main" component={Home} />
+            <Route exact path="/">
+              <Redirect to="/main" />
+            </Route>
+          </IonRouterOutlet>
+        )}
+      </IonReactRouter>
+    </IonApp >
+  )
+}
 
 export default App;
---
+++ src/store/store.ts
@@ -0,0 +1,27 @@
+import create from 'zustand';
+import { devtools } from 'zustand/middleware'
+
+interface CurrentUser  {
+    userId: string,
+    subscription?: string[]
+}
+
+interface StoreType {
+    currentUser?: CurrentUser;
+    setCurrentUser: (e: CurrentUser) => void;
+    currentUserReset: Function;
+    loading: boolean;
+}
+
+const store = (set: any): StoreType => ({
+    currentUser: undefined,
+    setCurrentUser(e) {
+        set((state: StoreType) => ({ currentUser: { ...state.currentUser, ...e } }));
+    },
+    currentUserReset() {
+        set((state: StoreType) => ({ ...state, currentUser: undefined }));
+    },
+    loading: true,
+})
+
+export const useStore = create(devtools(store));
