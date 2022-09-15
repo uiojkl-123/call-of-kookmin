7f38bf9e47029dbbc586f275155c7edb7a5d8de9
박도륜 <88328436+ryxxn@users.noreply.github.com>
2022-09-15 19:23:51 +0900

    -COKButton 컴포넌트 추가 - 편의점, 카페 장바구니 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { constants } from 'buffer';
 import { useHistory } from 'react-router';
 import { useStore } from '../store/store';
 import './Home.scss';
-import { idList } from '../static/constants';
+import { db } from '../static/constants';
 import { Link } from 'react-router-dom';
 
 const Home: React.FC = () => {
@@ -16,29 +16,32 @@ const Home: React.FC = () => {
   return (
     <IonPage className='homePage'>
       <IonContent fullscreen>
-            <header>
-              <h1>국민의 부름</h1>
-              <h1>{currentUser?.userName}</h1>
-            </header>
-        <div style={{ marginTop: '20px' }}>
-          {idList.map((value, index) => {
-            return <IonCard className='ion-padding' mode='ios' onClick={() => history.push('feedPage?pageNum=' + value)}>
-              게시글{index}
+        <header>
+          <h1>국민의 부름 {currentUser?.userName}</h1>
+        </header>
+
+        {/* {컴포넌트로 만들어서 param으로 게시글 id를 줍시다} */}ㄴ
+        <div style={{ marginTop: '70px' }}>
+          {db.map((value) => {
+            return <IonCard className='ion-padding' mode='ios' onClick={() => history.push('feedPage?pageNum=' + value.id)}>
+              게시글{value.id}
             </IonCard>
           })}
         </div>
 
-        <IonButton style={{
-          position: 'fixed',
-          zIndex: '10',
-          width: 'calc(100% - 32px)',
-          bottom: '16px',
-          fontSize: '20px',
-          padding: '0 8px 0 8px',
-          margin: '0 16px'
-        }}>
-          부르기
-        </IonButton>
+        <Link to='/call'>
+          <IonButton style={{
+            position: 'fixed',
+            zIndex: '10',
+            width: 'calc(100% - 32px)',
+            bottom: '16px',
+            fontSize: '20px',
+            padding: '0 8px 0 8px',
+            margin: '0 16px'
+          }}>
+            부르기
+          </IonButton>
+        </Link>
       </IonContent>
     </IonPage>
   );
