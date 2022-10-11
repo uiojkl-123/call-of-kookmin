373c589b2db52e5bd18d12511c0bc1a70101874c
uiojkl-123 <66120426+uiojkl-123@users.noreply.github.com>
2022-10-11 09:37:36 +0900

    내 부름이면 수락 못하고 진행중  버튼

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,6 +1,6 @@
 import { IonButton, IonCard, IonContent, IonIcon, IonItem, IonLabel, IonList, IonModal, IonToolbar, useIonAlert } from '@ionic/react'
 import { personCircleOutline, arrowBack } from 'ionicons/icons'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useHistory } from 'react-router'
 import { CallFeed } from '../pages/FeedPage'
 import { deleteCall } from '../serviece/call.service'
@@ -29,7 +29,16 @@ export const Feed: React.FC<FeedPageProps> = (props) => {
     const [presentAlert] = useIonAlert();
     const policy: string = '1. 튀지 말기\n2. 늦지 말기';
 
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [isMine, setIsMine] = useState<boolean>(false);
+
     const { currentUser } = useStore();
+
+    useEffect(() => {
+        if (currentUser?.userId === feed.writer) {
+            setIsMine(true);
+        }
+    }, [currentUser])
 
     const handleDelete = async () => {
         await deleteCall(feed.id)
@@ -37,7 +46,6 @@ export const Feed: React.FC<FeedPageProps> = (props) => {
         history.push('/main')
     }
 
-    const [isOpen, setIsOpen] = useState<boolean>(false);
 
 
     return (
@@ -125,36 +133,44 @@ export const Feed: React.FC<FeedPageProps> = (props) => {
 
 
 
-                    <div className='button'>
-                        <COKButton text={'요청 수락'} onClick={() => {
-                            presentAlert({
-                                mode: 'ios',
-                                header: '이용 약관',
-                                message: policy,
-                                cssClass: 'alert',
-                                buttons: [
-                                    {
-                                        text: '수락',
-                                        role: 'confirm',
-                                        cssClass: 'confirmBtn',
-                                        id: "open-modal",
-                                        handler: () => {
-                                            acceptFeed(feed.id, true, feed.writer);
-                                            setIsOpen(true);
-                                            //history.push('/accepting')
+                    {isMine ?
+                        <>
+                            {feed.isMatched ?
+                                <COKButton onClick={()=>{}} text='부름 진행중...'></COKButton>
+                                : null
+                            }
+                        </>
+                        :
+                        <div className='button'>
+                            <COKButton text={'요청 수락'} onClick={() => {
+                                presentAlert({
+                                    mode: 'ios',
+                                    header: '이용 약관',
+                                    message: policy,
+                                    cssClass: 'alert',
+                                    buttons: [
+                                        {
+                                            text: '수락',
+                                            role: 'confirm',
+                                            cssClass: 'confirmBtn',
+                                            id: "open-modal",
+                                            handler: () => {
+                                                acceptFeed(feed.id, true, feed.writer);
+                                                setIsOpen(true);
+                                                //history.push('/accepting')
+                                            },
                                         },
-                                    },
-                                    {
-                                        text: '취소',
-                                        role: 'cancle',
-                                        cssClass: 'cancleBtn',
-                                        handler: () => { },
-                                    },
-                                ],
-
-                            });
-                        }} />
-                    </div>
+                                        {
+                                            text: '취소',
+                                            role: 'cancle',
+                                            cssClass: 'cancleBtn',
+                                            handler: () => { },
+                                        },
+                                    ],
+                                });
+                            }} />
+                        </div>
+                    }
                 </>
                 : null}
             <IonModal trigger='open-modal' isOpen={isOpen} className='acceptingContainer'>
@@ -200,11 +216,11 @@ export const Feed: React.FC<FeedPageProps> = (props) => {
                                 <div>{feed.content}</div>
                             </div>
                             <div className='acceptingContentInfoBoxes'>
-                                <div>요청지</div> 
+                                <div>요청지</div>
                                 <div>{feed.location}</div>
                             </div>
                             <div className='acceptingContentInfoBoxes'>
-                                <div>팁</div> 
+                                <div>팁</div>
                                 <div>{feed.price} 원</div>
                             </div>
                         </div>
